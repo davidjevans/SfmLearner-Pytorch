d76445abb543b1c979201e8e5ef680697c83a62b
Ting-Che Lin <tingchel@andrew.cmu.edu>
2018-12-09 15:41:58 -0500

    Update train_script.sh
    to generate ground truth

--- train_script.sh
+++ train_script.sh
@@ -1,1 +1,1 @@
-python3 train.py /home/aws/datasets/kitti_combined/ -b4 -m0.2 -s0.1 --epoch-size 3000 --sequence-length 3 --log-output
+python3 train.py /home/aws/datasets/kitti_combined/ -b4 -m0.2 -s0.1 --epoch-size 3000 --sequence-length 3 --log-output --with-gt
